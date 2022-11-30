ca69d8f1a304feab7baaae4a176755efa940a417
pvinig <pvinig@gmail.com>
2022-11-30 20:47:45 -0300

    booting

--- copoLaranja/copoLaranja/settings.py
+++ copoLaranja/copoLaranja/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'suco',  
-        'USER': 'root',  
+        'USER': '',  
         'PASSWORD': '@T1nyt1nykey',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
